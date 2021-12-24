ae76174851033f2123cf4ac6277f7c8e9b2c854f
ixodes <ixodesrd@gmail.com>
2021-12-24 17:57:58 +0300

    Bouncing Ball solved

---
+++ src/test/java/by/ixodes/codewars/katas/kyu6/BouncingBallTest.java
@@ -0,0 +1,27 @@
+package by.ixodes.codewars.katas.kyu6;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @autor ixodes
+ * @since 12/24/21
+ */
+
+public class BouncingBallTest {
+
+    @Test
+    public void test1() {
+        assertEquals(3, BouncingBalls.bouncingBall(3.0, 0.66, 1.5));
+    }
+    @Test
+    public void test2() {
+        assertEquals(15, BouncingBalls.bouncingBall(30.0, 0.66, 1.5));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(1, BouncingBalls.bouncingBall(2.0, 0.5, 1.0));
+    }
+}
